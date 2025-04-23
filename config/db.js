f4bb5da6e40785ce6724ba536ce73bfe765ecd2e
Ashutosh-kumar-code <ashu9908kumar@gmail.com>
2025-04-23 21:59:24 +0530

    api changes

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {  });
         // await mongoose.connect("mongodb+srv://ashu9908kumar:X3zUEe5EIj1SjVHG@cluster0.7hwh3hb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", { });
-       
+           
         console.log('MongoDB Connected');
     } catch (error) {
         console.error('Database connection failed', error);
